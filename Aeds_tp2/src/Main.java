228811b694e8227d702e692ab655dd529acb04f9
kozukiIaguin <iagogfalcao@gmail.com>
2024-08-22 14:25:45 -0300

    falta iterativo e cm inserção

--- Aeds_tp2/src/Main.java
+++ Aeds_tp2/src/Main.java
@@ -1,33 +1,73 @@
 import Sorting.*;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        int n = 10; // Length of the array
+        Item[] arr;
+
+        // Create instances of sorting algorithms
         BubbleSort bubble = new BubbleSort();
-        InsertionSort insertion = new  InsertionSort();
+        InsertionSort insertion = new InsertionSort();
         SelectionSort selection = new SelectionSort();
         QuickSort quick = new QuickSort();
-        Item[] arr = {
-            new Item(64),
-            new Item(34),
-            new Item(25),
-            new Item(12),
-            new Item(22),
-            new Item(11),
-            new Item(90)
-        };
-         int n = arr.length;
-        // bubble.bubble_sort(arr, n);
-        // System.out.println("Bubble sorted array: ");
-        // Utils.printArray(arr, n);
-        // insertion.insertion_sort(arr);
-        // System.out.println("Insertion sorted array: ");
+
+        // Test BubbleSort
+        arr = generateRandomArray(n);
+        System.out.println("Original array:");
         Utils.printArray(arr, n);
-        // selection.selection_sort(arr);
-        // System.out.println("Selection sorted array: ");
-        // Utils.printArray(arr, n);
-        System.out.println("Iteractive quick sort: ");
-        quick.quick_sort(arr, 0, arr.length-1);
+        bubble.bubble_sort(arr, n);
+        System.out.println("Bubble sorted array:");
+        Utils.printArray(arr, n);
+
+        // Test InsertionSort
+        arr = generateRandomArray(n);
+        System.out.println("\nOriginal array:");
+        Utils.printArray(arr, n);
+        insertion.insertion_sort(arr);
+        System.out.println("Insertion sorted array:");
+        Utils.printArray(arr, n);
+
+        // Test SelectionSort
+        arr = generateRandomArray(n);
+        System.out.println("\nOriginal array:");
+        Utils.printArray(arr, n);
+        selection.selection_sort(arr);
+        System.out.println("Selection sorted array:");
+        Utils.printArray(arr, n);
+
+        // Test QuickSort (Standard)
+        arr = generateRandomArray(n);
+        System.out.println("\nOriginal array:");
+        Utils.printArray(arr, n);
+        quick.quick_sort(arr, 0, arr.length - 1);
+        System.out.println("Quick sorted array:");
+        Utils.printArray(arr, n);
+
+        // Test QuickSort with median of three
+        arr = generateRandomArray(n);
+        System.out.println("\nOriginal array:");
+        Utils.printArray(arr, n);
+        quick.three_median(arr);
+        System.out.println("Three median quick sorted array:");
+        Utils.printArray(arr, n);
+
+        // Test QuickSort with median of five
+        arr = generateRandomArray(n);
+        System.out.println("\nOriginal array:");
+        Utils.printArray(arr, n);
+        quick.five_median(arr);
+        System.out.println("Five median quick sorted array:");
         Utils.printArray(arr, n);
     }
-    
+
+    // Generates an array of random Items with values between 1 and 100
+    private static Item[] generateRandomArray(int size) {
+        Random rand = new Random();
+        Item[] arr = new Item[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = new Item(rand.nextInt(100) + 1);
+        }
+        return arr;
+    }
 }
